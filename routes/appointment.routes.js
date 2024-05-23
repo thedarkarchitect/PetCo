be7287b1c640c7aae0812554b2b245f41e341324
thedarkarchitect <romanreynolds240@gmail.com>
2024-05-23 15:32:12 +0300

    routes spacing

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -6,12 +6,15 @@ import { isAdmin, isUser } from "../utils/middleware.js";
 const appointmentRouter = new Router()
 
 appointmentRouter.get("/user-appointments/:ownerId", [verifyToken, isUser], getUserAppointments);
+
 appointmentRouter.get("/", [verifyToken, isAdmin], getAppointments);
+
 appointmentRouter.get("/get-appointment/:id", [verifyToken, isAdmin], getAppointmentById);
 
 appointmentRouter.post("/createAppointment", [verifyToken, isUser], createAppointment);
 
 appointmentRouter.patch("/update-appointment/:id", updateAppointment);
+
 appointmentRouter.delete("/delete-appointment/:id", [verifyToken, isAdmin], deleteAppointment);
 
 export default appointmentRouter;
--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -6,12 +6,15 @@ import { isAdmin, isUser } from "../utils/middleware.js";
 const petRouter = new Router();
 
 petRouter.get("/get-pet/:petId", [verifyToken, isAdmin], getPetById);//done
+
 petRouter.get("/", [verifyToken, isAdmin], getPets);//done
+
 petRouter.get("/get-owner-pet/:ownerId", [verifyToken, isAdmin], getOwnerPets);
 
 petRouter.post("/create-pet", [verifyToken, isUser], createPet);
 
 petRouter.patch("/update-pet/:petId", [verifyToken, isAdmin], updatePet);
+
 petRouter.delete("/delete-pet/:petId", [verifyToken, isAdmin], deletePet);
 
 export default petRouter;
